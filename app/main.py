7db4fdead8d9fdb3f01c6ff58f567dfdaecb04dd
bastosgabriel312 <bastosgabriel312@gmail.com>
2024-09-09 22:01:46 -0300

    feat: projeto rate-sync base

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 # app/main.py
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.routes import router as movie_router
 
 app = FastAPI()
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://rate-sync-ionic.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(movie_router, prefix="/api/v1")
